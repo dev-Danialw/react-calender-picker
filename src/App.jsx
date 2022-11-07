3c531ffb1d99674dcb3074c4bba70d5aba123951
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-07 21:49:08 +0500

    styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled, { css } from "styled-components";
 import "./App.css";
 
 const calendarDates = Array(31)
@@ -36,38 +37,89 @@ function App() {
 
   return (
     <>
-      <div className="date-chooser">
-        <button
-          className="date-chooser-button"
-          onClick={() => setChosenType("start")}
-        >
+      <StyledDateChooser>
+        <StyledDateChooserButton onClick={() => setChosenType("start")}>
           Start Date <span>{selectedDate}</span>
-        </button>
-        <button
-          className="date-chooser-button"
-          onClick={() => setChosenType("end")}
-        >
+        </StyledDateChooserButton>
+        <StyledDateChooserButton onClick={() => setChosenType("end")}>
           End Date <span>{endDate}</span>
-        </button>
-      </div>
+        </StyledDateChooserButton>
+      </StyledDateChooser>
 
-      <div className="calendar">
+      <StyledCalendar>
         {calendarDates.map((day, index) => {
           const dayNumber = day + 1;
+          const isSelected =
+            dayNumber === selectedDate || dayNumber === endDate;
 
           return (
-            <div
+            <StyledCalendarDay
               key={index}
-              className="calendar-day"
+              isSelected={isSelected}
               onClick={() => handleDateClick(dayNumber)}
             >
               {dayNumber}
-            </div>
+            </StyledCalendarDay>
           );
         })}
-      </div>
+      </StyledCalendar>
     </>
   );
 }
 
 export default App;
+
+const StyledDateChooser = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const StyledDateChooserButton = styled.button`
+  color: #0b204c;
+  text-transform: uppercase;
+  flex: 1;
+  padding: 15px;
+  background: none;
+  cursor: pointer;
+  border: none;
+  border-bottom: 2px solid rgba(11, 32, 76, 0.2);
+  outline: none;
+  border-color: ${(props) => (props.isChoosing ? "#0b204c" : "none")};
+  span {
+    display: block;
+    min-height: 60px;
+    font-size: 50px;
+  }
+`;
+
+const StyledCalendar = styled.div`
+  max-width: 400px;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  background: #0b204c;
+  color: #fff;
+  padding: 20px;
+`;
+
+const StyledCalendarDay = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  transition: 0.3s ease background;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: #8096c1;
+  background: none;
+
+  ${(props) =>
+    props.isSelected &&
+    css`
+      background: #1a1a1a !important;
+      color: #eee;
+    `}
+`;
