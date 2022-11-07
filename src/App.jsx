6a1a0cdb0449c0a7dc8573ebeca0f3f494aa9ee6
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-07 14:39:50 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+
+const calendarDates = Array(31)
+  .fill(0)
+  .map((e, i) => i);
+
+function App() {
+  return (
+    <>
+      <div className="date-chooser">
+        <button className="date-chooser-button">
+          Start Date <span>0</span>
+        </button>
+        <button className="date-chooser-button">
+          End Date <span>0</span>
+        </button>
+      </div>
+
+      <div className="calendar">
+        {calendarDates.map((day, index) => {
+          return <div className="calendar-day">{day + 1}</div>;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
